627ae7ec7211b4733b039bbe6b3a8aef965d2d0c
fravaralg <fravaralg@alum.us.es>
2022-03-08 12:07:35 +0100

    Final

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const port = process.env.PORT || 8080;
 
 app.use("/",express.static('public'));
 
-app.get("/time", (req,res)=>{
-    console.log("Requested / route")
+app.get("/faces", (req,res)=>{
+    console.log("Requested /faces route")
     res.send("<html><body><h1>"+cool()+"</h1></body></html>");
+});
+
+app.get("/time", (req,res)=>{
+    console.log("Requested /time route")
+    res.send("<html><body><h1>"+new Date()+"</h1></body></html>");
 });
 
 
