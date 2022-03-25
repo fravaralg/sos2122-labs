456e60fdfaa7848f6b23ffe928ddb7ca2add9b3c
fravaralg <fravaralg@alum.us.es>
2022-03-25 11:21:50 +0100

    update code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 
 const app = express();
@@ -7,9 +6,8 @@ const port = process.env.PORT || 8080;
 
 app.use("/",express.static('public'));
 
-app.get("/caritas", (req,res)=>{
-    console.log("Requested /faces route")
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
+app.get("/api", (req,res)=>{
+    res.send({ result: true});
 });
 
 app.get("/time", (req,res)=>{
